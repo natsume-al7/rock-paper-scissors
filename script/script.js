753b5dc5ac585f0039cbf36ef1eab37af5316d14
Natsume <natsume.al7@gmail.com>
2023-05-14 15:53:36 -0700

    Implement game function to simulate 5 round game

--- script/script.js
+++ script/script.js
@@ -14,31 +14,64 @@ function playRound (playerSelection, computerSelection) {
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     if (playerSelection === 'Rock') {
         if (computerSelection === 'Rock') {
-            return 'Tie! Rock cancels Rock';
+            console.log('Tie! Rock cancels Rock');
+            return 0;
         } else if (computerSelection === 'Paper') {
-            return 'You lose! Paper beats Rock';
+            console.log('You lose! Paper beats Rock');
+            return 2;
         } else {
-            return 'You win! Rock beats Scissors';
+            console.log('You win! Rock beats Scissors');
+            return 1;
         }
     }
 
     if (playerSelection === 'Paper') {
         if (computerSelection === 'Rock') {
-            return 'You win! Paper beats Rock';
+            console.log('You win! Paper beats Rock');
+            return 1;
         } else if (computerSelection === 'Paper') {
-            return 'Tie! Paper cancels Paper';
+            console.log('Tie! Paper cancels Paper');
+            return 0;
         } else {
-            return 'You lose! Scissors beats Paper';
+            console.log('You lose! Scissors beats Paper');
+            return 2;
         }
     }
 
     if (playerSelection === 'Scissors') {
         if (computerSelection === 'Rock') {
-            return 'You lose! Rock beats Scissors';
+            console.log('You lose! Rock beats Scissors');
+            return 2;
         } else if (computerSelection === 'Paper') {
-            return 'You win! Scissors beats Paper';
+            console.log('You win! Scissors beats Paper')
+            return 1;
         } else {
-            return 'Tie! Scissors cancels Scissors';
+            console.log('Tie! Scissors cancels Scissors');
+            return 0;
         }
     }
 }
+
+function game() {
+    let playerWins = 0;
+    let  computerWins = 0;
+    for (i = 0; i < 5; i++) {
+        const playerSelection = prompt('Rock, paper, or scissors: ');
+        const computerSelection = getComputerChoice();
+        let roundResult = playRound(playerSelection, computerSelection);
+        if (roundResult === 1) {
+            playerWins++;
+        } else if (roundResult === 2) {
+            computerWins++;
+        }
+    }
+    if (playerWins > computerWins) {
+        return `You win! Score: (Player) ${playerWins} - (Computer) ${computerWins}`;
+    } else if (playerWins < computerWins) {
+        return `You lose! Score: (Player) ${playerWins} - (Computer) ${computerWins}`;
+    } else {
+        return `Tie! Score: (Player) ${playerWins} - (Computer) ${computerWins}`
+    }
+}
+
+console.log(game());
