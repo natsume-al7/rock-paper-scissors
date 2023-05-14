116a96827b9c95bb9ae8fcc660250ce6635517f8
Natsume <natsume.al7@gmail.com>
2023-05-14 15:37:32 -0700

    Implement playRound function to simulate round

--- script/script.js
+++ script/script.js
@@ -10,4 +10,35 @@ function getComputerChoice() {
     }
 }
 
-let test = getComputerChoice();
+function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (playerSelection === 'Rock') {
+        if (computerSelection === 'Rock') {
+            return 'Tie! Rock cancels Rock';
+        } else if (computerSelection === 'Paper') {
+            return 'You lose! Paper beats Rock';
+        } else {
+            return 'You win! Rock beats Scissors';
+        }
+    }
+
+    if (playerSelection === 'Paper') {
+        if (computerSelection === 'Rock') {
+            return 'You win! Paper beats Rock';
+        } else if (computerSelection === 'Paper') {
+            return 'Tie! Paper cancels Paper';
+        } else {
+            return 'You lose! Scissors beats Paper';
+        }
+    }
+
+    if (playerSelection === 'Scissors') {
+        if (computerSelection === 'Rock') {
+            return 'You lose! Rock beats Scissors';
+        } else if (computerSelection === 'Paper') {
+            return 'You win! Scissors beats Paper';
+        } else {
+            return 'Tie! Scissors cancels Scissors';
+        }
+    }
+}
